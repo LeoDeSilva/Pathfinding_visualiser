d4537cba69be7fc94e53aee40fe642c57c464c52
leoDesilva <leo.desilvafamily@gmail.com>
2021-03-11 18:51:55 +0000

    updated

--- pathfinding.py
+++ pathfinding.py
@@ -1,9 +1,8 @@
 import pygame
-import math
-from queue import *
 from queue import PriorityQueue
 
-WIDTH = 780
+#WIDTH = 780
+WIDTH = 1530
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
 pygame.display.set_caption("A* Path Finding Algorithm")
 
@@ -21,6 +20,7 @@ ORANGE = (255,165,0)
 GREY = (128,128,128)
 TURQUOISE = (64,224,208)
 
+#<----------------Square on Grid----------------------->
 class Spot:
     def __init__(self,row,col,width,total_rows):
         self.row = row
@@ -32,9 +32,7 @@ class Spot:
         self.width = width
         self.total_rows = total_rows
 
-    def get_pos(self):
-        return self.row, self.col
-
+    def get_pos(self): return self.row, self.col
     def is_closed(self): return self.color == RED
     def is_open(self): return self.color == GREEN
     def is_barrier(self): return self.color == BLACK
@@ -63,133 +61,90 @@ class Spot:
         if self.row > 0 and not grid[self.row][self.col-1].is_barrier():#LEFT
             self.neighbors.append(grid[self.row][self.col-1])
 
-    def __lt__(self,other):
-        return False
-    
+    def __lt__(self,other): return False
+
+#<----------------H Function----------------------->
 def h(p1,p2):
     x1,y1 = p1
     x2,y2 = p2
     return abs(x1-x2) + abs(y1-y2)
 
+
+#<----------------Create path from nodes----------------------->
 def reconstruct_path(came_from, current, draw):
     while current in came_from:
         current = came_from[current]
         if not current.is_start() and not current.is_end(): current.make_path()
         draw()
 
+#<----------------Create path from nodes----------------------->
 def create_path(path,rows,grid):
     for cur in path:
         if not cur.is_start() and not cur.is_end(): cur.make_path()
         draw(WIN,grid,rows,WIDTH)
 
-def BFS(grid,start, end,rows):
-    frontier = []
-    frontier.append(start)
-    explored = []
-    queue = [[start]]
-    while queue:
-        current_node = frontier.pop()
-        print(queue)
-        path = queue.pop()
-        node = path[-1]
-        #current_node.make_open()
-        for node in frontier:  
-            if not node.is_start() and not node.is_end(): node.make_open()
-
-        for node in explored:
-            if not node.is_start() and not node.is_end(): node.make_closed()
-        if current_node in explored: continue
-        if path[-1] == end: 
-            print("FHOUND")
-            create_path(path,rows,grid)
-            print(path)
-            return path
-            #for node in frontier:
-            #    if not node.is_start() and not node.is_end(): 
-            #        node.make_path()
-            return True
-        for neighbor in current_node.neighbors:
-            frontier.append(neighbor)
-            if neighbor in frontier:
-                new_path = list(path)
-                new_path.append(neighbor)
-                queue.append(new_path)
-        explored.append(current_node)
-        draw(WIN,grid,rows,WIDTH)
-
+#<---------------- Breath First Search----------------------->
 def bfs(graph_to_search, start, end, rows):
     queue = [[start]]
     visited = set()
 
     while queue:
-        # Gets the first path in the queue
         path = queue.pop(0)
-
-        # Gets the last node in the path
         vertex = path[-1]
         for node in visited:
             if not node.is_start() and not node.is_end(): node.make_closed()
-        # Checks if we got to the end
         if vertex == end:
             print("HOUNS")
-
             create_path(path,rows,graph_to_search)
             return path
-        # We check if the current node is already in the visited nodes set in order not to recheck it
         elif vertex not in visited:
-            #if not vertex.is_start() and not vertex.is_end(): vertex.make_open()
-            # enumerate all adjacent nodes, construct a new path and push it into the queue
             for current_neighbour in vertex.neighbors:
                 if not current_neighbour.is_start() and not current_neighbour.is_end(): current_neighbour.make_open()
                 new_path = list(path)
                 new_path.append(current_neighbour)
                 queue.append(new_path)
-
-            # Mark the vertex as visited
             visited.add(vertex)
         draw(WIN, graph_to_search, rows, WIDTH)
     print("NOT HOUNS")
+
+#<---------------- Depth First Search----------------------->
 def dfs(graph_to_search, start, end, rows):
     queue = [[start]]
     visited = set()
     a = True
     bf = False
     while queue:
-        # Gets the first path in the queue
         path = queue.pop()
-
-        # Gets the last node in the path
         vertex = path[-1]
         for node in visited:
             if not node.is_start() and not node.is_end(): node.make_closed()
-        # Checks if we got to the end
         if vertex == end:
             print("HOUNS")
 
             create_path(path,rows,graph_to_search)
             return path
-        # We check if the current node is already in the visited nodes set in order not to recheck it
         elif vertex not in visited:
-            #if not vertex.is_start() and not vertex.is_end(): vertex.make_open()
-            # enumerate all adjacent nodes, construct a new path and push it into the queue
             for current_neighbour in vertex.neighbors:
                 if not current_neighbour.is_start() and not current_neighbour.is_end(): current_neighbour.make_open()
                 new_path = list(path)
                 new_path.append(current_neighbour)
                 queue.append(new_path)
-
-            # Mark the vertex as visited
             visited.add(vertex)
         draw(WIN, graph_to_search, rows, WIDTH)
     print("NOT HOUNS")
 
+
+#<---------------- Border around Screen----------------------->
 def make_border(graph):
     for j in range(len(graph)):
         for i in range(len(graph[j])):
             if i == 0 or j == 0 or i == len(graph[j])-1 or j == len(graph)-1:
                 graph[j][i].make_barrier()
+
+
+#<---------------- A* Pathfinding----------------------->
 def algorithm(draw, grid, start, end):
-    count = 0
+    count = 0 
     open_set = PriorityQueue()
     open_set.put((0, count, start))
     came_from = {}
@@ -229,6 +184,8 @@ def algorithm(draw, grid, start, end):
             current.make_closed()
     return False
 
+
+#<---------------- A* Pathfinding----------------------->
 def make_grid(rows, width):
     grid = []
     gap = width // rows
@@ -240,6 +197,8 @@ def make_grid(rows, width):
 
     return grid
 
+
+#<---------------- Draw the grid lines----------------------->
 def draw_grid(win,rows,width):
     gap = width // rows
     for i in range(rows):
@@ -247,6 +206,8 @@ def draw_grid(win,rows,width):
         for j in range(rows):
             pygame.draw.line(win,GREY,(j*gap,0),(j*gap,width))
 
+
+#<---------------- Draw The Grid and Squares----------------------->
 def draw(win,grid,rows,width):
     win.fill(WHITE)
     for row in grid:
@@ -256,14 +217,16 @@ def draw(win,grid,rows,width):
     pygame.display.update()
 
 
+#<----------------Draw the Grid and Squares without display ----------------------->
 def draw_main(win,grid,rows,width):
-
     win.fill(WHITE)
     for row in grid:
         for spot in row:
             spot.draw(win)
     draw_grid(win,rows, width)
 
+
+#<----------------Get Mouse Clicked Tile ----------------------->
 def get_clicked_pos(pos,rows,width):
     gap = width // rows
     y,x = pos
@@ -271,31 +234,33 @@ def get_clicked_pos(pos,rows,width):
     col = x // gap
     return row, col
 
+#<---------------- Main Function----------------------->
 def main(win, width):
     ROWS = 30
     grid = make_grid(ROWS, width)
     make_border(grid)
     start = None
     end = None
-
     a = True
     bf = False  
     df = False
-
     run = True
     started = False
     while run:
+        #Draw to the Screen
         draw_main(win, grid, ROWS, width)
-
         if a == True: pygame.draw.rect(WIN, TURQUOISE, button)
         elif bf == True: pygame.draw.rect(WIN, GREEN, button)
         else: pygame.draw.rect(WIN, PURPLE, button)
         pygame.display.update()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
             if started:
                 continue
+
+            #Place Tile
             if pygame.mouse.get_pressed()[0]:
                 pos = pygame.mouse.get_pos()
                 row,col = get_clicked_pos(pos,ROWS, width)
@@ -308,6 +273,8 @@ def main(win, width):
                     end.make_end()
                 elif spot != end and spot != start:
                     spot.make_barrier()
+            
+            #Delete Tile 
             elif pygame.mouse.get_pressed()[2]:
                 pos = pygame.mouse.get_pos()
                 row,col = get_clicked_pos(pos,ROWS, width)
@@ -317,19 +284,24 @@ def main(win, width):
                     start = None
                 elif spot == end:
                     end = None
+                
             if event.type == pygame.KEYDOWN:
+                #Start Algorithm
                 if event.key == pygame.K_SPACE and not started:
                     for row in grid:
                         for spot in row:
                             spot.update_neighbors(grid)
-                    #May need try except as if start not placed, gives error
                     if a == True: algorithm(lambda: draw(win,grid,ROWS,width), grid, start, end)
                     elif bf == True: bfs(grid,start,end,ROWS)
                     elif df == True: dfs(grid,start,end,ROWS)
+
+                #Reset Algorithm
                 if event.key == pygame.K_c:
                     for row in grid:
                         for spot in row:
                             if not spot.is_barrier() and not spot.is_start() and not spot.is_end(): spot.reset()
+
+                #Clear Board
                 if event.key == pygame.K_x:
                     for row in grid:
                         for spot in row:
@@ -337,6 +309,8 @@ def main(win, width):
                     start =  None
                     end = None
                     make_border(grid)
+
+            #Switch Algorithm Button
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_pos = event.pos
                 if button.collidepoint(mouse_pos):
